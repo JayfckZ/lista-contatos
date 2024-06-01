e22c09b20a551175158e995b29729d3e6a1729a1
Jay <joaomarcelo1015ja@gmail.com>
2024-06-01 19:07:11 -0300

    setup finalizado

---
+++ src/styles/indext.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle, styled } from "styled-components"
+
+export const EstiloGlobal = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`
